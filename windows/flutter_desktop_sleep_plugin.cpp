d5f21d712eac6396307e423fd51ef22d6baecacb
bikram40 <bikram.meher@magnigeeks.com>
2022-09-13 19:45:03 +0530

    windows side done

--- windows/flutter_desktop_sleep_plugin.cpp
+++ windows/flutter_desktop_sleep_plugin.cpp
@@ -10,21 +10,36 @@
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
 
+#include <map>
 #include <memory>
 #include <sstream>
 
 namespace flutter_desktop_sleep {
+    using flutter::EncodableMap;
+    using flutter::EncodableValue;
+
+    static LRESULT CALLBACK WindowCloseWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+    static WNDPROC oldProc;
+    static std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
 
 // static
 void FlutterDesktopSleepPlugin::RegisterWithRegistrar(
     flutter::PluginRegistrarWindows *registrar) {
+      HWND handle = GetActiveWindow();
+            oldProc = reinterpret_cast<WNDPROC>(GetWindowLongPtr(handle, GWLP_WNDPROC));
+            SetWindowLongPtr(handle, GWLP_WNDPROC, (LONG_PTR)WindowCloseWndProc);
   auto channel =
       std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
           registrar->messenger(), "flutter_desktop_sleep",
           &flutter::StandardMethodCodec::GetInstance());
-
+  channel_ = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+      registrar->messenger(),
+      "flutter_desktop_sleep",
+      &flutter::StandardMethodCodec::GetInstance());
+  ;
   auto plugin = std::make_unique<FlutterDesktopSleepPlugin>();
 
+        plugin->SetWindow(handle);
   channel->SetMethodCallHandler(
       [plugin_pointer = plugin.get()](const auto &call, auto result) {
         plugin_pointer->HandleMethodCall(call, std::move(result));
@@ -36,6 +51,16 @@ void FlutterDesktopSleepPlugin::RegisterWithRegistrar(
 FlutterDesktopSleepPlugin::FlutterDesktopSleepPlugin() {}
 
 FlutterDesktopSleepPlugin::~FlutterDesktopSleepPlugin() {}
+
+void FlutterDesktopSleepPlugin::SetWindow(HWND handle)
+    {
+        m_windowHandle = handle;
+    }
+
+    HWND FlutterDesktopSleepPlugin::GetWindow()
+    {
+        return m_windowHandle;
+    }
 
 void FlutterDesktopSleepPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
@@ -55,5 +80,32 @@ void FlutterDesktopSleepPlugin::HandleMethodCall(
     result->NotImplemented();
   }
 }
+ LRESULT CALLBACK
+    WindowCloseWndProc(HWND hWnd, UINT iMessage, WPARAM wparam, LPARAM lparam)
+    {
+        if (iMessage == WM_CLOSE)
+        {
+            auto args = std::make_unique<flutter::EncodableValue>(nullptr);
+            channel_->InvokeMethod("onWindowClose", std::move(args));
+            return 0;
+        }
+        if (iMessage == WM_POWERBROADCAST)
+        {
+            if (wparam == PBT_APMSUSPEND)
+            {
+                auto args = std::make_unique<flutter::EncodableValue>("sleep");
+                channel_->InvokeMethod("onWindowsSleep", std::move(args));
+            }
+            else if (wparam == PBT_APMRESUMEAUTOMATIC)
+            {
+                auto args = std::make_unique<flutter::EncodableValue>("woke_up");
+                channel_->InvokeMethod("onWindowsSleep", std::move(args));
+            }
+
+            return TRUE;
+        }
+
+        return oldProc(hWnd, iMessage, wparam, lparam);
+    }
 
 }  // namespace flutter_desktop_sleep
--- windows/flutter_desktop_sleep_plugin.h
+++ windows/flutter_desktop_sleep_plugin.h
@@ -6,27 +6,32 @@
 
 #include <memory>
 
-namespace flutter_desktop_sleep {
+namespace flutter_desktop_sleep
+{
 
-class FlutterDesktopSleepPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+    class FlutterDesktopSleepPlugin : public flutter::Plugin
+    {
+    public:
+        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  FlutterDesktopSleepPlugin();
+        FlutterDesktopSleepPlugin();
 
-  virtual ~FlutterDesktopSleepPlugin();
+        virtual ~FlutterDesktopSleepPlugin();
 
-  // Disallow copy and assign.
-  FlutterDesktopSleepPlugin(const FlutterDesktopSleepPlugin&) = delete;
-  FlutterDesktopSleepPlugin& operator=(const FlutterDesktopSleepPlugin&) = delete;
+        void SetWindow(HWND handle);
+        HWND GetWindow();
+        // Disallow copy and assign.
+        FlutterDesktopSleepPlugin(const FlutterDesktopSleepPlugin &) = delete;
+        FlutterDesktopSleepPlugin &operator=(const FlutterDesktopSleepPlugin &) = delete;
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
+    private:
+        HWND m_windowHandle;
+        // Called when a method is called on this plugin's channel from Dart.
+        void HandleMethodCall(
+            const flutter::MethodCall<flutter::EncodableValue> &method_call,
+            std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+    };
 
-}  // namespace flutter_desktop_sleep
+} // namespace flutter_desktop_sleep
 
-#endif  // FLUTTER_PLUGIN_FLUTTER_DESKTOP_SLEEP_PLUGIN_H_
+#endif // FLUTTER_PLUGIN_FLUTTER_DESKTOP_SLEEP_PLUGIN_H_
